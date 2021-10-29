0dc94e86850c921d916048a04b83ed76de8e98d3
0xbinibini <wdnmd22@protonmail.com>
2021-10-29 23:13:15 +0800

    del everything

--- main/evtx/24-25.cmd
+++
@@ -1,5 +0,0 @@
-title remote连接日志24-25 && ..\..\tools\Logparser\logparser.exe -i:EVT -o:DATAGRID "select TimeGenerated,EventID,EventType,Strings,ComputerName,SID from %arg%\Microsoft-Windows-TerminalServices-RemoteConnectionManager%%4Operational.evtx where %eventid% %sqltime%"
-rem || ..\..\tools\Logparser\logparser.exe -i:EVT -o:DATAGRID "select TimeGenerated,EventID,EventType,Strings,ComputerName,SID from '%arg%\Microsoft-Windows-TerminalServices-RemoteConnectionManager Operational.evtx' where %eventid% %sqltime%"
-
-
-pause
